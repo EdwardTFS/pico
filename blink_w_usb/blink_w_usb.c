040f81c59d0e46319f97111e8888282ef265f8ac
Edward Görlich <goerlich_e@tfs.pl>
2023-05-16 20:36:21 +0200

    pico w blink with usb serial info

--- blink_w_usb/blink_w_usb.c
+++ blink_w_usb/blink_w_usb.c
@@ -4,14 +4,24 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
+// #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+#define SLEEP_TIME 1000
 
 int main() {
     stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(2000);
+        if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed");
+        return -1;
     }
-    return 0;
+    while (true) {
+        printf("ON\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(SLEEP_TIME);
+        printf("OFF\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(SLEEP_TIME);
+    }
 }
